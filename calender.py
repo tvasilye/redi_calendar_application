787c815bbc4a055ffca916ebb05ef864d1b16f2e
tvasilye <vasilyeva109053@gmail.com>
2024-10-16 18:44:02 +0200

    project description added

---
+++ calender.py
@@ -0,0 +1,6 @@
+# application description: 
+# a minimal calendar application.
+
+# The outcome would probably 
+# be that we can save dates in it and send some request to check 
+# with the calendar app if we have time or not.
